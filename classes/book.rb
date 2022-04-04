9cb4fbcb713e42c8e2799c0f4509ca874460aae7
VuDej <drvu47@gmail.com>
2022-04-04 16:14:14 +0200

    required changes

--- classes/book.rb
+++ classes/book.rb
@@ -1,8 +1,14 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rental
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rental = []
+  end
+
+  def add_rental(rental)
+    @rental.push(rental)
+    rental.book = self
   end
 end
--- classes/person.rb
+++ classes/person.rb
@@ -10,6 +10,12 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
+  end
+
+  def add_rental(rental)
+    @rental.push(rental)
+    rental.person = self
   end
 
   def correct_name
