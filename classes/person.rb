923b1fdcd4f59e0ad05344855f5bd271fc1deb54
VuDej <drvu47@gmail.com>
2022-04-04 13:36:15 +0200

    added logic in nameable.rb for capitalize and trim name and imported to persone.rb

--- classes/person.rb
+++ classes/person.rb
@@ -1,4 +1,4 @@
-require './nameable'
+require_relative './nameable'
 
 class Person < Nameable
   attr_reader :id
@@ -10,6 +10,10 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
